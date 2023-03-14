8f62dd4c761ce05066e4caa431e5b70ef10eaacd
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-03-14 17:19:03 +0200

    Sample to .images

--- predict.py
+++ predict.py
@@ -127,7 +127,7 @@ class Predictor(BasePredictor):
                 [prompt] * num_outputs,
                 num_inference_steps=num_inference_steps,
                 guidance_scale=guidance_scale,
-            )
+            ).images
 
         output_paths = []
         for i, image in enumerate(images):

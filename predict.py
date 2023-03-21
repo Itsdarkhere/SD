99f50824ddf3a4edf53e8a125f3c7ad3d3104aa7
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-03-21 12:51:24 +0200

    Fix syntax

--- predict.py
+++ predict.py
@@ -95,7 +95,7 @@ class Predictor(BasePredictor):
         loaded_learned_embeds = torch.load(embeds_path, map_location="cpu")
         trained_token = list(loaded_learned_embeds.keys())[0]
         embeds = loaded_learned_embeds[trained_token]
-        dtype = text_encoder.get_input_embeddings().weight.dtype
+        dtype = self.text_encoder.get_input_embeddings().weight.dtype
         embeds.to(dtype)
         token = token if token is not None else trained_token
         num_added_tokens = tokenizer.add_tokens(token)
@@ -103,9 +103,9 @@ class Predictor(BasePredictor):
         if num_added_tokens == 0:
             raise ValueError(f"The tokenizer already contains the token {token}.")
 
-        text_encoder.resize_token_embeddings(len(tokenizer))
+        self.text_encoder.resize_token_embeddings(len(tokenizer))
         token_id = tokenizer.convert_tokens_to_ids(token)
-        text_encoder.get_input_embeddings().weight.data[token_id] = embeds
+        self.text_encoder.get_input_embeddings().weight.data[token_id] = embeds
 
         print("loading StableDiffusionPipeline with updated tokenizer and text_encoder")
         pipeline = StableDiffusionPipeline.from_pretrained(

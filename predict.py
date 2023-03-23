41d79bfad8a65ab384146da3535fe89e4dc82a30
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-03-23 18:13:31 +0200

    Try lavastyle

--- predict.py
+++ predict.py
@@ -82,21 +82,21 @@ class Predictor(BasePredictor):
 
         seed_everything(seed)
 
-        embeds_path = './bonzi.pt'
-        placeholder = '<bonzi>'
+        embeds_path = './Lavastyle.pt'
+        placeholder = '<Lavastyle>'
         
         # Load the learned concept
         loaded_learned_embeds = torch.load(embeds_path, map_location="cpu")
 
         # Separate the token and the embed
-        trained_token = list(loaded_learned_embeds.keys())[0]
-        embeds = loaded_learned_embeds[trained_token]
-        # string_to_token = loaded_learned_embeds['string_to_token']
-        # string_to_param = loaded_learned_embeds['string_to_param']
-        # trained_token = list(string_to_token.keys())[0]
-        # embeds = string_to_param[trained_token]
-        # embeds = embeds.detach()
-        # embeds = embeds[1]
+        # trained_token = list(loaded_learned_embeds.keys())[0]
+        # embeds = loaded_learned_embeds[trained_token]
+        string_to_token = loaded_learned_embeds['string_to_token']
+        string_to_param = loaded_learned_embeds['string_to_param']
+        trained_token = list(string_to_token.keys())[0]
+        embeds = string_to_param[trained_token]
+        embeds = embeds.detach()
+        embeds = embeds[1]
 
         # Convert the embed to the same dtype as the text_encoder
         dtype = self.text_encoder.get_input_embeddings().weight.dtype

5291ec3975eee219810642caa027ca918e19d160
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-03-27 12:09:49 +0300

    Idea

--- predict.py
+++ predict.py
@@ -83,7 +83,7 @@ class Predictor(BasePredictor):
         # seed_everything(seed)
 
         embeds_path = './Lavastyle.pt'
-        placeholder = '<Lavastyle>'
+        placeholder = '<LAVASTYLE>'
         
         # Load the learned concept
         loaded_learned_embeds = torch.load(embeds_path, map_location="cpu")
